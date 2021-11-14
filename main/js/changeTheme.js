c0827104b5c9a2b4a941e9815376196aaf4f8366
CDZ-Answers <88682294+CDZ-Answers@users.noreply.github.com>
2021-11-14 15:01:00 +0300

    Add files via upload

--- main/js/changeTheme.js
+++ main/js/changeTheme.js
@@ -13,16 +13,13 @@ function changeStylesheetBtn(chboxElementId) {
         iconColor = 'light';
     }
 
+    document.getElementById('moonImg').setAttribute('src', `https://cdz-answers.github.io/main/images/${iconColor}MoonStyle.png`);
+    document.getElementById('sunImg').setAttribute('src', `https://cdz-answers.github.io/main/images/${iconColor}SunStyle.png`);
+
     document.getElementById('mainStylesheet').setAttribute('href', `https://cdz-answers.github.io/main/css/main${stylesheet}.css`);
-    try {
-        document.getElementById('abouteStylesheet').setAttribute('href', `https://cdz-answers.github.io/main/css/aboutePage${stylesheet}.css`);
-    } catch {}
     try {
         document.getElementById('cdzStylesheet').setAttribute('href', `https://cdz-answers.github.io/cdz/css/cdz${stylesheet}.css`);
     } catch {}
-
-    document.getElementById('moonImg').setAttribute('src', `https://cdz-answers.github.io/main/images/${iconColor}MoonStyle.png`);
-    document.getElementById('sunImg').setAttribute('src', `https://cdz-answers.github.io/main/images/${iconColor}SunStyle.png`);
 
     document.cookie = `stylesheet=${stylesheet};path=/`;
 }
